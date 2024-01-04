95a4bfbd25db8dad36732730079d8d224dbf5212
takyannick <takam_yannick@yahoo.fr>
2024-01-04 16:56:45 +0100

    updating de data model draft with keys and updating the queries and datas of the 4 tables

--- 002-Ali AdventureWorks/Data Cleaning & Transformation/DIM_Product.sql
+++ 002-Ali AdventureWorks/Data Cleaning & Transformation/DIM_Product.sql
@@ -1,13 +1,15 @@
-SELECT TOP (1000) [ProductKey]
-      ,[ProductAlternateKey]
-      ,[ProductSubcategoryKey]
-      ,[EnglishProductName]
-      ,[StandardCost]
-      ,[Color]
-      ,[SafetyStockLevel]
-      ,[SizeRange]
-      ,[ModelName]
-      ,[StartDate]
-      ,[EndDate]
-      ,[Status]
-  FROM [AdventureWorksDW2019].[dbo].[DimProduct]
+SELECT TOP (1000) p.ProductKey
+      ,p.ProductAlternateKey as ProductItemCode
+	  ,ps.EnglishProductSubcategoryName as Prod_SubCategory
+	  ,pc.EnglishProductCategoryName as Prod_Category
+      ,p.EnglishProductName as Product_Name
+      ,p.StandardCost
+      ,p.Color as Color
+      ,p.SafetyStockLevel
+      ,p.SizeRange
+      ,p.ModelName
+      ,p.StartDate
+      ,isnull(p.Status, 'Outdated') as Prod_Status
+  FROM (DimProduct as p left join DimProductSubcategory as ps on p.ProductSubcategoryKey = ps.ProductSubcategoryKey)
+  left join DimProductCategory as pc on ps.ProductCategoryKey = pc.ProductCategoryKey
+  order by ProductKey
--- 002-Ali AdventureWorks/Data Cleaning & Transformation/FACT_InternetSales.sql
+++ 002-Ali AdventureWorks/Data Cleaning & Transformation/FACT_InternetSales.sql
@@ -1,15 +1,15 @@
 SELECT 
-	  s.SalesOrderNumber
+	  s.SalesOrderNumber as Order_Nbr
 	  ,s.ProductKey
       ,s.OrderDateKey as OrderDateKey
       ,s.CustomerKey
       ,s.CurrencyKey
-      ,st.SalesTerritoryRegion
-	  ,st.SalesTerritoryCountry
+      ,st.SalesTerritoryRegion as Region
+	  ,st.SalesTerritoryCountry as Country
       ,s.OrderQuantity
       ,s.UnitPrice
       ,s.SalesAmount
-	  ,s.ProductStandardCost
+	  ,s.ProductStandardCost as Cost
 FROM FactInternetSales as s left join DimSalesTerritory as st on st.SalesTerritoryKey = s.SalesTerritoryKey
 where left(OrderDateKey, 4) >= year(getdate()) - 2 
 Order by s.SalesOrderNumber asc
